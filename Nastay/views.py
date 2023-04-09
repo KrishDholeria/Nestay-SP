089ba1b74798b7bcce55cf2750b9754d9453bb79
KrishDholeria <96837857+KrishDholeria@users.noreply.github.com>
2023-04-10 01:54:23 +0530

    Delete Nastay directory

--- Nastay/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    return render(request,"index.html")
-def services(request):
-    return render(request,"services.html")
-
-
